b5128c9e5f51ee1cb718d072290198dc503fd00d
Erica Pantoja <ericapantoja@Ericas-MBP.lan>
2023-09-20 20:54:49 -0500

    adding meta tags

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,12 +1,22 @@
 
-import './App.css';
+import React from 'react';
+import Header from '.components/Header';
+import Nav from '.components/Nav';
+import Main from '.components/Main';
+import Footer from '.components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Header/>
+    <Nav/>
+    <Main/>
+    <Footer/>
+  
       <h1>Little Lemon</h1>
       
-    </div>
+    </>
   );
 }
 
